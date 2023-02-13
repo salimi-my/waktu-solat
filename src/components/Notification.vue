35c404159248f0604554e85a132318e32d0bbd72
Mohamad Salimi <contact@salimi.my>
2023-02-13 16:54:28 +0800

    Fix notification ref

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,6 +1,6 @@
 <template>
   <TransitionRoot as="template" :show="enable">
-    <Dialog as="div" class="relative z-10" @close="enable = false">
+    <Dialog as="div" class="relative z-10" @close="sendEnableEvent">
       <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100"
         leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
@@ -26,7 +26,7 @@
               <div class="bg-white px-4 py-3 flex justify-center items-center">
                 <button type="button"
                   class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-gray-50 px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:border focus:ring-0 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  @click="enable = false" ref="cancelButtonRef">Tutup</button>
+                  @click="sendEnableEvent" ref="cancelButtonRef">Tutup</button>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -43,4 +43,7 @@ import { InformationCircleIcon } from '@heroicons/vue/24/outline'
 const props = defineProps({
   enable: Boolean
 })
+
+const emit = defineEmits(["enable-event"]);
+const sendEnableEvent = () => emit("enable-event");
 </script>
